cff23258034d4c4430224a30237f73174911d23d
Sourav Tripathi <sourabh12abh@gmail.com>
2025-12-12 16:28:04 +0530

    fourth commit

--- script.js
+++ script.js
@@ -1,45 +1,81 @@
-// create a element
-// appending a child
+// sync  ->  ek time pe ek hi kaam kr skte hai jaise purane nokia ke phones
+// async ->  ek time pe kyi cheeze ho ske modern mobile phones
+
+// js ka nature sync hota hai or hm ab use async krenga
+
+
+// setTimeout -> delay
+
+
+// setTimeout(function(){
+//     console.log('hello');
+// }, 2000)
 
 
 
-// var h1 = document.createElement('h1');
-// console.log(h1);
+// printing order -> hello1 hello3 hello2 
+
+// console.log('hello1')
+
+// setTimeout(function(){
+//     console.log('hello2');
+// }, 2000)
+
+// console.log('hello3')
 
 
-// var btn = document.querySelector('button');
+
+
+// printing order -> hello2  hello3 hello1 
+
+// setTimeout(function(){
+//     console.log('hello1');
+// }, 5000)
+
+// setTimeout(function(){
+//     console.log('hello2');
+// }, 2000)
+
+// setTimeout(function(){
+//     console.log('hello3');
+// }, 4000)
+
+
+
+
+
+// var user = 'sourav'
+
+// var btn = document.querySelector('button')
+// var h1 = document.querySelector('h1')
 
 // btn.addEventListener('click', function(){
+//     h1.innerHTML = `Hello I am ${user}`
 
-//     var h1 = document.createElement('h1');
-//     h1.innerHTML = "hello from js"
-
-//     console.log(h1);
+//     setTimeout(function(){
+//         h1.innerHTML = `how are you ${user}`
+//     }, 2000)
 // })
 
 
 
 
-
-// var h1 = document.createElement('h1');
-
-// h1.innerHTML = "Hello from JS"
-
-// var main = document.querySelector('main');
-
-// main.appendChild(h1);
+// setInterval(function(){
+//     console.log('hello ji!')
+// }, 2000)  // hr 2 sec ke interval ke baad hello ji aayega console pe
 
 
 
+// var a = 0;
 
+// var sI = setInterval(function(){
+//     a++
+//     console.log(a)
+// }, 100);
 
-// var btn = document.createElement('button')
-
-// btn.innerHTML = 'download'
-
-// var main = document.querySelector('main')
-
-// main.appendChild(btn)
+// setTimeout(function(){
+//     clearInterval(sI)
+// }, 5000);
 
 
 
@@ -48,83 +84,32 @@
 
 
 
-// random boxes project
-
-// var main = document.querySelector('main');
-// var btn = document.querySelector('button');
 
 
-// btn.addEventListener('click', function(){
-//     var box = document.createElement('div');
-//     box.style.height = '50px';
-//     box.style.width = '50px';
+// download button project
 
-//     box.style.position = 'absolute';
-
-
-//     var c1 = Math.floor(Math.random()*256)
-//     var c2 = Math.floor(Math.random()*256)
-//     var c3 = Math.floor(Math.random()*256)
-
-//     box.style.backgroundColor = `rgb(${c1},${c2},${c3})`
-
-//     var x = Math.random()*100;
-//     var y = Math.random()*100;
-
-//     var r = Math.random()*360
-
-
-//     box.style.left = x+'%'
-//     box.style.top = y+'%'
-
-//     box.style.rotate = r+'deg'
-
-//     main.appendChild(box);
-// })
-
-
-
-
-
-
-// random lines project
-
-
-
-var arr = ['hello ji', 'juice peelo mausambi ka', 'imandari chalat ha babu', 'Bhupendra jogi', 'asla hm bhi rakhte hai pehlwan', 'sher cheetah hi kehde']
-var main = document.querySelector('main');
+var grow = 0;
+var h2 = document.querySelector('h2')
+var inner = document.querySelector('.inner')
 var btn = document.querySelector('button');
 
-
 btn.addEventListener('click', function(){
-    var h1 = document.createElement('h1')
+    
+    btn.style.pointerEvents = 'none'  // ek baar btn ko daba diya toh uske baad dubara nhi dba skte
 
-    var a = Math.floor(Math.random()*arr.length)
+    var num = 50 + Math.floor(Math.random()*50)
+    
+    console.log('Your file will be downloaded in', num/10, 'seconds')
 
-    h1.innerHTML = arr[a]
+    var dn = setInterval(() => {
+        grow++
+        inner.style.width = grow + '%';
+        h2.innerHTML = grow + '%'
+    }, num);
 
-    h1.style.position = 'absolute';
-
-
-    // var c1 = Math.floor(Math.random()*256)
-    // var c2 = Math.floor(Math.random()*256)
-    // var c3 = Math.floor(Math.random()*256)
-
-    // box.style.backgroundColor = `rgb(${c1},${c2},${c3})`
-
-
-    var x = Math.random()*80;
-    var y = Math.random()*80;
-
-    var r = Math.random()*360
-    var scl = Math.random()*3
-
-
-    h1.style.left = x+'%'
-    h1.style.top = y+'%'
-
-    h1.style.rotate = r+'deg'
-    h1.style.scale = scl
-
-    main.appendChild(h1);
+    setTimeout(() => {
+        clearInterval(dn)
+        btn.innerHTML = 'Downloaded'
+        btn.style.opacity = 0.5
+    }, num*100);
 })
